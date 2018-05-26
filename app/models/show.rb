1f0f277a1b117f489ef931d3d0acfdf59c2feb24
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-26 15:31:31 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,14 +4,14 @@ class Show < ActiveRecord::Base
    self.order(rating: :asc).first
   end
 
-  def most_popular_show
+  def self.most_popular_show
   end
 
   def self.lowest_rating
     self.order(rating: :desc).first
   end
 
-  def leastpopular_show
+  def self.leastpopular_show
   self.order(rating: :desc).first[:name]
   end
 
@@ -19,9 +19,9 @@ class Show < ActiveRecord::Base
      self.sum(rating:)
   end
 
-  def popular_shows
+  def self.popular_shows
   end
 
-  def shows_by_alphabetical_order
+  def self.shows_by_alphabetical_order
   end
 end
