b07f2d54477e7047a24c8c431b7f5e4c9eab38b6
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-26 15:00:16 +0000

    automatically backed up by learn

--- app/models/shows_controler.rb
+++ app/models/shows_controler.rb
@@ -1,26 +1,26 @@
-
+class Show
 def highest_rating
-  Show.order(rating: :asc).first
+#  Show.order(rating: :asc).first
 end
 
 def most_popular_show
-
 end
 
 def lowest_rating
-  Show.order(rating: :desc).first
+#  Show.order(rating: :desc).first
 end
 
 def leastpopular_show
-Show.order(rating: :desc).first[:name]
+#Show.order(rating: :desc).first[:name]
 end
 
 def ratings_sum
-  Show.sum(rating:)
+#  Show.sum(rating:)
 end
 
 def popular_shows
 end
 
 def shows_by_alphabetical_order
+end
 end
