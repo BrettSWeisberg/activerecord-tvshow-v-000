1337544e7d4636c28c37dcd64fd1356a90b92c8e
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-26 15:57:18 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -21,7 +21,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.popular_shows
-    self.where("rrating > 5").order(rating: :desc)
+    self.where("rating > 5").order(rating: :desc)
   end
 
   def self.shows_by_alphabetical_order
