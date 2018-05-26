64fe54b74e939607a7d22deb7ca60b2e18e1c958
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-26 15:32:14 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,27 +1,30 @@
 class Show < ActiveRecord::Base
 
   def self.highest_rating
-   self.order(rating: :asc).first
+    self.order(rating: :asc).first
   end
 
   def self.most_popular_show
+
   end
 
   def self.lowest_rating
-    self.order(rating: :desc).first
+    #self.order(rating: :desc).first
   end
 
   def self.leastpopular_show
-  self.order(rating: :desc).first[:name]
+      #self.order(rating: :desc).first[:name]
   end
 
   def self.ratings_sum
-     self.sum(rating:)
+      #self.sum(rating:)
   end
 
   def self.popular_shows
   end
 
   def self.shows_by_alphabetical_order
+
   end
+  
 end
